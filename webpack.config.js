7eddcc7bce808364c3d7fbe51ff04d1b351b737a
TE-Good <tom.good1990@gmail.com>
2019-12-19 17:39:15 +0000

    inital commit

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve('dist'),
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.css$/, loader: ['style-loader', 'css-loader'] },
+      { test: /\.s(a|c)ss$/, loader: ['style-loader', 'css-loader', 'sass-loader'] }
+    ]
+  },
+  devServer: {
+    contentBase: path.resolve('src'),
+    hot: true,
+    open: true,
+    port: 8000,
+    watchContentBase: true,
+    historyApiFallback: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      filename: 'index.html',
+      inject: 'body'
+    })
+  ],
+  // NEW FOR TONE.JS
+  resolve: {
+    root: __dirname,
+    // for webpack 1:
+    modulesDirectories: ["path/to/Tone.js/"],
+    // for webpack 2:
+    modules: ["path/to/Tone.js/"]
+  }
+}
