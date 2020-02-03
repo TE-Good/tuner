4316333eb3587a5abc1641e386bd06ea41faa44b
TE-Good <tom.good1990@gmail.com>
2020-02-03 16:41:44 +0000

    logger added

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ const bodyParser = require('body-parser')
 
 const router = require('./config/router')
 const { PORT, dbURI } = require('./config/enviroment')
+const logger = require('./lib/logger')
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 
 app.use(express.static(`${__dirname}/dist`))
 
 app.use(bodyParser.json())
+
+app.use(logger)
 
 app.use('/api', router)
 
