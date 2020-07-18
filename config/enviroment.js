31687d7187c46fc14f5113d4dc435b2cb4cdc46e
TE-Good <tom.good1990@gmail.com>
2020-07-18 08:21:03 +0100

    removed old backend

--- config/enviroment.js
+++
@@ -1,7 +0,0 @@
-const PORT = process.env.PORT || 8000
-
-const dbURI = process.env.MONGO_URI || 'mongodb://localhost/tuner'
-
-const secret = 'sauce'
-
-module.exports = { PORT, dbURI, secret }
--- models/tuning.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const tuningSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  notes: [{ type: String, required: true }]
-})
-
-module.exports = mongoose.model('Tuning', tuningSchema)
